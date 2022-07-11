95557ea09fd14d8a06954d9f80a79d1941e1df9f
yonathansantosa <yonathansantosa@gmail.com>
2022-07-11 07:16:36 +0700

    fix navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -56,109 +56,13 @@
           </a>
         </div>
         <div class="border-b-2 pb-4 text-lg font-semibold">Sebagai {{ ucwords(session()->get('auth_wlha.username.0')) }}</div>
+        {{-- Use icon from here
+            https://heroicons.com/ choose from the outline one --}}
         <div>
-          {{-- Accounts --}}
-          @if (in_array(session()->get('auth_wlha.id_level.0'), [1, 2]))
-            @php
-              $active = in_array('accounts', $routes_name) ? true : false;
-              $svgd = 'M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z';
-            @endphp
-            <x-navitem href="{{ route('accounts.index') }}" :active=$active :svgd=$svgd :viewbox='0 0 448 512'>
-              Accounts
-            </x-navitem>
-          @endif
-          {{-- Kepegawaian --}}
-          @if (in_array(session()->get('auth_wlha.id_level.0'), [1, 2]))
-            @php
-              $active = in_array('pegawai', $routes_name) ? true : false;
-              $svgd = 'M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4';
-            @endphp
-            <x-navitem href="{{ route('pegawai.index') }}" :active=$active :svgd=$svgd>
-              Kepegawaian
-            </x-navitem>
-          @endif
-          {{-- Penghuni --}}
-          @if (in_array(session()->get('auth_wlha.id_level.0'), [1, 2, 3]))
-            @php
-              $active = in_array('penghuni', $routes_name) ? true : false;
-              $svgd = 'M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z';
-            @endphp
-            <x-navitem href="{{ route('penghuni.index') }}" :active=$active :svgd=$svgd>
-              Penghuni
-            </x-navitem>
-          @endif
-          {{-- Rekam Medis --}}
-          @if (in_array(session()->get('auth_wlha.id_level.0'), [1, 2, 3, 4]))
-            @php
-              $active = in_array('rekmed', $routes_name) ? true : false;
-              $svgd = 'M7 12l3-3 3 3 4-4M8 21l4-4 4 4M3 4h18M4 4h16v12a1 1 0 01-1 1H5a1 1 0 01-1-1V4z';
-            @endphp
-            <x-navitem href="{{ route('rekmed.index') }}" :active=$active :svgd=$svgd>
-              Rekam Medis
-            </x-navitem>
-          @endif
-          {{-- Farmasi --}}
-          @if (in_array(session()->get('auth_wlha.id_level.0'), [1, 2, 6]))
-            @php
-              $active = in_array('farmasi', $routes_name) ? true : false;
-              $svgd = 'M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10';
-            @endphp
-            <x-navitem href="{{ route('farmasi.index') }}" :active=$active :svgd=$svgd>
-              Farmasi
-            </x-navitem>
-          @endif
-          {{-- Fisioterapi --}}
-          @if (in_array(session()->get('auth_wlha.id_level.0'), [1, 2]))
-            @php
-              $active = in_array('fisioterapi', $routes_name) ? true : false;
-              $svgd = 'M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4';
-            @endphp
-            <x-navitem href="#" :active=$active :svgd=$svgd>
-              Fisioterapi
-            </x-navitem>
-          @endif
-          {{-- Mobilitas --}}
-          @if (in_array(session()->get('auth_wlha.id_level.0'), [1, 2, 3, 4]))
-            @php
-              $active = in_array('mobilitas', $routes_name) ? true : false;
-              $svgd = 'M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z';
-            @endphp
-            <x-navitem href="{{ route('mobilitas.index') }}" :active=$active :svgd=$svgd>
-              Mobilitas
-            </x-navitem>
-          @endif
-          {{-- Inventaris --}}
-          @if (in_array(session()->get('auth_wlha.id_level.0'), [1, 2]))
-            @php
-              $active = in_array('inventaris', $routes_name) ? true : false;
-              $svgd = 'M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z';
-            @endphp
-            <x-navitem href="#" :active=$active :svgd=$svgd>
-              Inventaris
-            </x-navitem>
-          @endif
+          @include('layouts.navigationlist')
         </div>
       </div>
       {{-- LOGOUT --}}
-      <div class="mt-auto flex flex-col space-y-4">
-        <a href="{{ route('auth.logout') }}"
-          class="mt-1 flex items-center space-x-4 rounded-md border-2 border-white bg-white p-2 text-lg font-semibold text-red-400 transition duration-200 hover:border-red-600 hover:text-red-600">
-          <svg fill="none" class="mr-4 h-6 w-6"
-        <a href="{{ route('askep.index') }}"
-          class="flex mt-1 p-2 items-center space-x-4 font-semibold text-lg rounded-md border-2 transition duration-200
-                    @if (in_array('inventaris', $routes_name)) {{ 'bg-indigo-600 text-white border-indigo-600' }}
-                    @else
-                        {{ 'bg-white border-white text-gray-400 hover:text-indigo-600 hover:border-indigo-600' }} @endif
-                    ">
-          <svg class="w-6 h-6 mr-4" fill="none"
-            stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-              d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z">
-            </path>
-          </svg>
-          Asuhan Keperawatan
-        </a>
-      </div>
       <div class="mt-auto flex flex-col space-y-4">
         <a href="{{ route('auth.logout') }}"
           class="mt-1 flex items-center space-x-4 p-2 text-lg font-semibold text-red-400 transition duration-200 hover:text-red-600">
--- routes/web.php
+++ routes/web.php
@@ -42,7 +42,7 @@ Route::group(['prefix' => 'auth', 'as' => 'auth.', 'namespace' => 'Auth'], funct
 
 // home
 Route::get('home', function () {
-    return view('home.index');
+    return view('home.index', $data = ['routes_name' => ['']]);
 });
 
 
@@ -84,13 +84,12 @@ Route::group(['prefix' => 'penghuni', 'as' => 'penghuni.', 'middleware' => ['rol
     Route::post('/data', [PenghuniController::class, 'data_penghuni'])->name('data');
 });
 
+Route::group(['prefix' => 'askep', 'as' => 'askep.'], function () {
+    Route::get('/', [AsuhanKeperawatanController::class, 'penghuni'])->name('index');
+    Route::post('/data', [AsuhanKeperawatanController::class, 'data_penghuni'])->name('data');
+});
 
 Route::group(['prefix' => 'rekmed', 'as' => 'rekmed.', 'middleware' => ['role:admin,manajer,perawat,assisten']], function () {
-
-    Route::group(['prefix' => 'askep', 'as' => 'askep.'], function () {
-        Route::get('/', [AsuhanKeperawatanController::class, 'penghuni'])->name('index');
-        Route::post('/data', [AsuhanKeperawatanController::class, 'data_penghuni'])->name('data');
-    });
 
     Route::get('/', [RekamMedisController::class, 'penghuni'])->name('index');
     Route::post('/data', [RekamMedisController::class, 'data_penghuni'])->name('data');
