a47307df6c1c9dc2868a78d71dd8913e1fa37cd8
yonathansantosa <yonathansantosa@gmail.com>
2023-04-11 12:49:10 +0700

    fixing positioning of navbar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -55,132 +55,12 @@
             </svg>
           </a>
         </div>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-        <a href="{{ route('pegawai.index') }}"
-          class="flex mt-1 p-2 items-center space-x-4 font-semibold text-lg rounded-md border-2 transition duration-200
-                    @if (in_array('pegawai', $routes_name)) {{ 'bg-indigo-600 text-white border-indigo-600' }}
-                    @else
-                        {{ 'bg-white border-white text-gray-400 hover:text-indigo-600 hover:border-indigo-600' }} @endif
-                    ">
-          <svg class="w-6 h-6 mr-4"
-            fill="none"
-            stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-              d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4">
-            </path>
-          </svg>
-          Kepegawaian
-        </a>
-        <a href="{{ route('penghuni.index') }}"
-          class="flex mt-1 p-2 items-center space-x-4 font-semibold text-lg rounded-md border-2 transition duration-200
-                    @if (in_array('penghuni', $routes_name)) {{ 'bg-indigo-600 text-white border-indigo-600' }}
-                    @else
-                        {{ 'bg-white border-white text-gray-400 hover:text-indigo-600 hover:border-indigo-600' }} @endif
-                    ">
-          <svg class="w-6 h-6 mr-4" fill="none"
-            stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-              d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z">
-            </path>
-          </svg>
-          Penghuni
-        </a>
-        <a href="{{ route('rekmed.index') }}"
-          class="flex mt-1 p-2 items-center space-x-4 font-semibold text-lg rounded-md border-2 transition duration-200
-                    @if (in_array('rekmed', $routes_name)) {{ 'bg-indigo-600 text-white border-indigo-600' }}
-                    @else
-                        {{ 'bg-white border-white text-gray-400 hover:text-indigo-600 hover:border-indigo-600' }} @endif
-                    ">
-          <svg class="w-6 h-6 mr-4" fill="none"
-            stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-              d="M7 12l3-3 3 3 4-4M8 21l4-4 4 4M3 4h18M4 4h16v12a1 1 0 01-1 1H5a1 1 0 01-1-1V4z"></path>
-          </svg>
-          Rekam Medis
-        </a>
-        <a href="{{ route('farmasi.index') }}"
-          class="flex mt-1 p-2 items-center space-x-4 font-semibold text-lg rounded-md border-2 transition duration-200
-                    @if (in_array('farmasi', $routes_name)) {{ 'bg-indigo-600 text-white border-indigo-600' }}
-                    @else
-                        {{ 'bg-white border-white text-gray-400 hover:text-indigo-600 hover:border-indigo-600' }} @endif
-                    ">
-          <svg class="w-6 h-6 mr-4" fill="none"
-            stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-              d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10">
-            </path>
-          </svg>
-          Farmasi
-        </a>
-        <a href="#"
-          class="flex mt-1 p-2 items-center space-x-4 font-semibold text-lg rounded-md border-2 transition duration-200
-                    @if (in_array('fisioterapi', $routes_name)) {{ 'bg-indigo-600 text-white border-indigo-600' }}
-                    @else
-                        {{ 'bg-white border-white text-gray-400 hover:text-indigo-600 hover:border-indigo-600' }} @endif
-                    ">
-          <svg class="w-6 h-6 mr-4" fill="none"
-            stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-              d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"></path>
-          </svg>
-          Fisioterapi
-        </a>
-        <a href="{{ route('mobilitas.index') }}"
-          class="flex mt-1 p-2 items-center space-x-4 font-semibold text-lg rounded-md border-2 transition duration-200
-                    @if (in_array('mobilitas', $routes_name)) {{ 'bg-indigo-600 text-white border-indigo-600' }}
-                    @else
-                        {{ 'bg-white border-white text-gray-400 hover:text-indigo-600 hover:border-indigo-600' }} @endif
-                    ">
-          <svg class="w-6 h-6 mr-4" fill="none"
-            stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-              d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z">
-            </path>
-          </svg>
-          Mobilitas
-        </a>
-        <a href="#"
-          class="flex mt-1 p-2 items-center space-x-4 font-semibold text-lg rounded-md border-2 transition duration-200
-                    @if (in_array('inventaris', $routes_name)) {{ 'bg-indigo-600 text-white border-indigo-600' }}
-                    @else
-                        {{ 'bg-white border-white text-gray-400 hover:text-indigo-600 hover:border-indigo-600' }} @endif
-                    ">
-          <svg class="w-6 h-6 mr-4" fill="none"
-            stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-              d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z">
-            </path>
-          </svg>
-          Inventarisasi
-        </a>
-        <a href="{{ route('askep.index') }}"
-          class="flex mt-1 p-2 items-center space-x-4 font-semibold text-lg rounded-md border-2 transition duration-200
-                    @if (in_array('inventaris', $routes_name)) {{ 'bg-indigo-600 text-white border-indigo-600' }}
-                    @else
-                        {{ 'bg-white border-white text-gray-400 hover:text-indigo-600 hover:border-indigo-600' }} @endif
-                    ">
-          <svg class="w-6 h-6 mr-4" fill="none"
-            stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-              d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z">
-            </path>
-          </svg>
-          Asuhan Keperawatan
-        </a>
-=======
->>>>>>> ac0c1d5b2fed0aefcede10fdb8805519f18ca419
         <div class="border-b-2 pb-4 text-lg font-semibold">Sebagai {{ ucwords(session()->get('auth_wlha.username.0')) }}</div>
         {{-- Use icon from here
             https://heroicons.com/ choose from the outline one --}}
         <div>
           @include('layouts.navigationlist')
         </div>
-<<<<<<< HEAD
-=======
->>>>>>> 4b1a7f466987bf81e4782f662e050f2b743da280
->>>>>>> ac0c1d5b2fed0aefcede10fdb8805519f18ca419
       </div>
       {{-- LOGOUT --}}
       <div class="mt-auto flex flex-col space-y-4">
@@ -200,10 +80,10 @@
 <!-- END: Side Navbar -->
 <div class="mb-96 flex h-full">
   <div class="h-full overflow-y-auto bg-indigo-50" :class="sidebarOpen ? 'w-full' : 'w-full'" id='content'>
-    {{ $slot }}
-    <div class="absolute left-[2vh] bottom-[2vh] bg-white p-[0.2vh] text-[2vh]">
-      &copy;TI Unika <br> Soegijapranata
+    <div class="z-40">
+      {{ $slot }}
     </div>
+    <div class="m-4"></div>
   </div>
 
   <script>
